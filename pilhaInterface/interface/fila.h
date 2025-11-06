c9abb1d70bf34d0824ed50fe42a54e2da2733047
Bruno Lima da Silva <ozamodas123@gmail.com>
2025-11-05 22:39:57 -0300

    implementando funcao 'retirar' da fila

--- pilhaInterface/interface/fila.h
+++ pilhaInterface/interface/fila.h
@@ -6,7 +6,7 @@ typedef struct fila Fila;
 
 //funcoes
 Fila* criar_fila();
-int verifica_vazio();
+int isvazio();
 void inserir(Fila* fila, int valor);
 int retirar(Fila* fila);
 void liberar(Fila* fila);
